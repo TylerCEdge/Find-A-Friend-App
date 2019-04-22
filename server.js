3be690006b65c873c2c97930d312f298cce03c1d
tyler <tylercedge@gmail.com>
2019-04-22 13:13:02 -0400

    html routing functionality added and tested successfully

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser')
+var path = require('path');
 
 var PORT = process.env.PORT || 8080;
 
@@ -12,6 +13,9 @@ app.use(bodyParser.json({ type: 'application/*+json' }))
 app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }))
 
 app.use(bodyParser.text({ type: 'text/html' }))
+
+require("./app/routing/htmlRoutes.js")(app);
+// require("./app/routing/apiRoutes")(app);
 
 
 app.listen(PORT, function() {
