a32ad672e01d1476630e042bb877af6d8f97d34d
tyler <tylercedge@gmail.com>
2019-04-22 14:57:53 -0400

    started building apiRoutes.js to use survey results to display your friend

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,80 +4,80 @@ var friends = [
     "name":"Ahmed",
     "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
     "scores":[
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
+        "5",
+        "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
       ]
   },
   {
     "name":"Archer",
     "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
     "scores":[
-        3,
-        2,
-        1,
-        5,
-        3,
-        4,
-        3,
-        1,
-        3,
-        2
+        "3",
+        "2",
+        "1",
+        "5",
+        "3",
+        "4",
+        "3",
+        "1",
+        "3",
+        "2"
       ]
   },
   {
     "name":"Rick",
     "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
     "scores":[
-        5,
-        5,
-        4,
-        4,
-        1,
-        1,
-        3,
-        4,
-        1,
-        4
+        "5",
+        "5",
+        "4",
+        "4",
+        "1",
+        "1",
+        "3",
+        "4",
+        "1",
+        "4"
       ]
   },
   {
     "name":"Morty",
     "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
     "scores":[
-        2,
-        2,
-        1,
-        5,
-        1,
-        3,
-        5,
-        2,
-        1,
-        5
+        "2",
+        "2",
+        "1",
+        "5",
+        "1",
+        "3",
+        "5",
+        "2",
+        "1",
+        "5"
       ]
   },
   {
     "name":"Peter",
     "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
     "scores":[
-        2,
-        2,
-        1,
-        5,
-        3,
-        4,
-        1,
-        3,
-        2,
-        4
+        "2",
+        "2",
+        "1",
+        "5",
+        "3",
+        "4",
+        "1",
+        "3",
+        "2",
+        "4"
       ]
   },
 ]
